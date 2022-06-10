7edcf9b7efd8f06acaf5887828cd282bc0a27fb6
Alfredo Munoz <kmstry214@gmail.com>
2022-06-10 13:58:14 -0500

    add svg

--- src/components/Navbar.js/Navbar.js
+++ src/components/Navbar.js/Navbar.js
@@ -48,11 +48,18 @@ const Navbar = () => {
           </button>
         </li>
         <Link to="/cart" className="hover:text-rose-600">
-          <CartIcon />
+          {/* <CartIcon /> */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-18 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
         </Link>
         <Link to={"/auth"} className="hover:text-green-500">
           {!isUserLoggedIn ? (
-            <UserIcon />
+            // <UserIcon />
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-18 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+          </svg>
+
           ) : (
             <div className="group relative dropdown cursor-pointer font-medium tracking-wide">
               <p className="hover:text-green-500">{user.username}</p>
